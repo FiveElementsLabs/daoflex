ecb84f32c2f83d65f509b7b6dbce87fb5a07297e
Nicolas <nicolas@racchi.com>
2022-04-09 12:26:18 +0200

    feat: removed lens

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { DAppProvider } from '@usedapp/core';
-import { ApolloProvider } from '@apollo/client';
 import { SharedStateProvider } from './context/store';
 import { theme } from '@chakra-ui/react';
 import Router from './Router';
 import dAppConfig from './lib/dAppConfig';
-import client from './lib/apolloClient';
 
 export default function App() {
   return (
     <SharedStateProvider>
       <ChakraProvider theme={theme}>
         <DAppProvider config={dAppConfig}>
-          <ApolloProvider client={client}>
-            <Router />
-          </ApolloProvider>
+          <Router />
         </DAppProvider>
       </ChakraProvider>
     </SharedStateProvider>
--- src/app.config.ts
+++ src/app.config.ts
@@ -10,11 +10,6 @@ const config = {
     rpcUrl: 'https://rpc-mumbai.matic.today',
     explorer: 'https://mumbai.polygonscan.com/',
   },
-  lens: {
-    apiUrl: 'https://api-mumbai.lens.dev/',
-    publicationMetadataVersion: '1.0.0',
-    appId: 'testing-daohacks',
-  },
 };
 
 export default config;
