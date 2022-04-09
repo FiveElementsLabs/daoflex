736f7dbfc0a2ad3c399affe43d17236dad4222b0
Paolo Antonio Rossi <paoloantoniorossi@gmail.com>
2022-04-09 12:29:42 +0200

    feat(unlock): basic unlock script integration

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,28 @@
 import DaoList from '../components/home/DaoList';
 
 export default function Home() {
+
+  function handleClick(e: any) {
+    e.preventDefault();
+    console.log('The link was clicked.');
+    (window as any).unlockProtocol && (window as any).unlockProtocol.loadCheckoutModal()
+  }
+
   return (
     <>
       <DaoList />
+      <div>
+
+      <h1>Unlock Demo</h1>
+<p className="unlock-content locked">Unlock Demo is currently locked 🔒 </p>
+<p className="unlock-content unlocked">Unlock Demo is currently unlocked 🎉</p>
+<p className="unlock-content locked" onClick={handleClick}><button className="button">
+    Unlock!
+  </button></p>
+
+
+      </div>
+      
     </>
   );
 }
