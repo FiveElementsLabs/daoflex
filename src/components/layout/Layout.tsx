a0a1c7ddab5a50ee8da4c3b45f68cba7453e088f
Nicolas <nicolas@racchi.com>
2022-04-10 10:17:47 +0200

    feat: distribute v1 test

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,13 +1,16 @@
 import { useEffect } from 'react';
 import { Box, Container, SlideFade } from '@chakra-ui/react';
 import { Outlet, useLocation } from 'react-router-dom';
+import { useSharedState } from '../../context/store';
 import { useWallet } from '../../hooks/useWallet';
 import { useLoadData } from '../../hooks/useLoadData';
 
 import Navbar from './Navbar';
 import Footer from './Footer';
+import LoadingState from '../LoadingState';
 
 export default function Layout() {
+  const [{ loading }] = useSharedState();
   const { autoLoginWallet } = useWallet();
   const { loadData } = useLoadData();
   const location = useLocation();
@@ -24,6 +27,8 @@ export default function Layout() {
 
   return (
     <Container maxW='container.lg'>
+      {loading && <LoadingState />}
+
       <Navbar />
 
       <Box mt={4}>
--- src/hooks/useDistribute.ts
+++ src/hooks/useDistribute.ts
@@ -5,14 +5,43 @@ export function useDistribute() {
   const [{ account }] = useSharedState();
 
   const DAIx = '0x745861AeD1EEe363b4AaA5F1994Be40b1e05Ff90';
+  const recipient = account;
 
   // TODO: handle distribution via smart contract or keeper script
-  const url = 'https://rinkeby.infura.io/v3/61a9f83ab0ae4873818b67960409b7fe';
-  const customHttpProvider = new ethers.providers.JsonRpcProvider(url);
-
+  // We distribute tokens from the dev wallet to the user
   const distribute = async (amount: number) => {
-    // TODO: use ethers to send transaction to signed-in user
+    try {
+      const url = 'https://rinkeby.infura.io/v3/61a9f83ab0ae4873818b67960409b7fe';
+      const customHttpProvider = new ethers.providers.JsonRpcProvider(url);
+
+      const wallet = new ethers.Wallet(process.env.REACT_APP_PRIVATE_KEY || '');
+      const signer = wallet.connect(customHttpProvider);
+      const currentGasPrice = await customHttpProvider.getGasPrice();
+      const gasPrice = ethers.utils.hexlify(parseInt(currentGasPrice.toString()));
+      console.log('gas price: ', gasPrice);
+
+      const contract = new ethers.Contract(DAIx, DAIx_ABI, signer);
+      const amountFinal = ethers.utils.parseUnits(amount.toString(), 18);
+      console.log('amount final: ', amountFinal);
+
+      const tx = await contract.transfer(recipient, amountFinal);
+      console.log('tx: ', tx);
+    } catch (err: any) {
+      console.log(err?.message);
+    }
   };
 
   return { distribute };
 }
+
+const DAIx_ABI = [
+  { stateMutability: 'payable', type: 'fallback' },
+  {
+    inputs: [{ internalType: 'address', name: 'initialAddress', type: 'address' }],
+    name: 'initializeProxy',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  { stateMutability: 'payable', type: 'receive' },
+];
