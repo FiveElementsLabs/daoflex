f46769d8c3c60e2aefcd875c5eca60e44d6a9fca
Nicolas <nicolas@racchi.com>
2022-04-10 12:32:11 +0200

    fix: working balance

--- src/components/dao/Sidebar.tsx
+++ src/components/dao/Sidebar.tsx
@@ -1,6 +1,26 @@
+import { useState, useEffect } from 'react';
 import { Box, Image, Text, useColorModeValue } from '@chakra-ui/react';
+import { useBalance } from '../../hooks/useBalance';
 
 export default function Sidebar({ dao }: { dao: any }) {
+  const { getBalance } = useBalance();
+  const [balance, setBalance] = useState(0);
+
+  const fetchBalance = async () => {
+    while (true) {
+      setBalance((await getBalance()) || 0);
+      await new Promise(resolve => setTimeout(resolve, 10000));
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      fetchBalance();
+    })();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Box
       px={4}
@@ -8,7 +28,7 @@ export default function Sidebar({ dao }: { dao: any }) {
       rounded='xl'
       shadow='sm'
       borderWidth='1px'
-      textAlign='center'
+      textAlign='left'
       w='full'
       borderColor={useColorModeValue('gray.300', 'gray.700')}
       _hover={{ borderColor: useColorModeValue('gray.400', 'gray.600') }}
@@ -18,8 +38,14 @@ export default function Sidebar({ dao }: { dao: any }) {
         <Image src={dao.image} alt={dao.name} mb={4} w='100px' />
       </Box>
       <Text fontSize='md' fontWeight='medium' mt={2} opacity='0.8'>
-        {/* //TODO: better number mapping function */}
-        {Math.floor(11000 / 1000)}K members
+        Your points: {balance}
+      </Text>
+      <Text mt={2} opacity='0.8'>
+        Points required for the next tier: 3000
+      </Text>
+
+      <Text fontSize='md' fontWeight='medium' mt={2} opacity='0.8'>
+        {dao.description}
       </Text>
     </Box>
   );
--- src/hooks/useBalance.ts
+++ src/hooks/useBalance.ts
@@ -10,7 +10,7 @@ export function useBalance() {
     try {
       const contract = new ethers.Contract(DAIx, DAIx_ABI, provider);
       const balance = await contract.balanceOf(account);
-      console.log('balance: ', balance);
+      return Number(ethers.utils.formatEther(balance));
     } catch (err: any) {
       console.log(err?.message);
     }
@@ -20,13 +20,224 @@ export function useBalance() {
 }
 
 const DAIx_ABI = [
-  { stateMutability: 'payable', type: 'fallback' },
   {
-    inputs: [{ internalType: 'address', name: 'initialAddress', type: 'address' }],
-    name: 'initializeProxy',
-    outputs: [],
+    constant: true,
+    inputs: [],
+    name: 'name',
+    outputs: [
+      {
+        name: '',
+        type: 'string',
+      },
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: '_spender',
+        type: 'address',
+      },
+      {
+        name: '_value',
+        type: 'uint256',
+      },
+    ],
+    name: 'approve',
+    outputs: [
+      {
+        name: '',
+        type: 'bool',
+      },
+    ],
+    payable: false,
     stateMutability: 'nonpayable',
     type: 'function',
   },
-  { stateMutability: 'payable', type: 'receive' },
+  {
+    constant: true,
+    inputs: [],
+    name: 'totalSupply',
+    outputs: [
+      {
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: '_from',
+        type: 'address',
+      },
+      {
+        name: '_to',
+        type: 'address',
+      },
+      {
+        name: '_value',
+        type: 'uint256',
+      },
+    ],
+    name: 'transferFrom',
+    outputs: [
+      {
+        name: '',
+        type: 'bool',
+      },
+    ],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'decimals',
+    outputs: [
+      {
+        name: '',
+        type: 'uint8',
+      },
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: '_owner',
+        type: 'address',
+      },
+    ],
+    name: 'balanceOf',
+    outputs: [
+      {
+        name: 'balance',
+        type: 'uint256',
+      },
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'symbol',
+    outputs: [
+      {
+        name: '',
+        type: 'string',
+      },
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: '_to',
+        type: 'address',
+      },
+      {
+        name: '_value',
+        type: 'uint256',
+      },
+    ],
+    name: 'transfer',
+    outputs: [
+      {
+        name: '',
+        type: 'bool',
+      },
+    ],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: '_owner',
+        type: 'address',
+      },
+      {
+        name: '_spender',
+        type: 'address',
+      },
+    ],
+    name: 'allowance',
+    outputs: [
+      {
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    payable: true,
+    stateMutability: 'payable',
+    type: 'fallback',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        name: 'spender',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        name: 'value',
+        type: 'uint256',
+      },
+    ],
+    name: 'Approval',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        name: 'from',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        name: 'to',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        name: 'value',
+        type: 'uint256',
+      },
+    ],
+    name: 'Transfer',
+    type: 'event',
+  },
 ];
