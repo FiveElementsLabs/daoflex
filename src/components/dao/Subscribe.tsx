a964d7875aa65b7cee7b6a5948aceb620c36556a
Nicolas <nicolas@racchi.com>
2022-04-10 11:55:24 +0200

    feat: subscribe component

--- src/components/dao/Subscribe.tsx
+++ src/components/dao/Subscribe.tsx
@@ -1,11 +1,11 @@
-import { Box, Button, Flex, Text, useColorModeValue } from '@chakra-ui/react';
-import { Sparkle } from 'phosphor-react';
+import { Box, Button, Flex, Grid, Text, useColorModeValue, VStack } from '@chakra-ui/react';
+import { Sparkle, Chats } from 'phosphor-react';
 import { useDistribute } from '../../hooks/useDistribute';
 import { useCreateNewFlow } from '../../hooks/useCreateFlow';
 import { useLoading } from '../../hooks/useLoading';
 
 export default function Subscribe({ dao }: { dao: any }) {
-  const { createNewFlow } = useCreateNewFlow();
+  // const { createNewFlow } = useCreateNewFlow();
   const { distribute } = useDistribute();
   const { loading } = useLoading();
 
@@ -13,8 +13,9 @@ export default function Subscribe({ dao }: { dao: any }) {
     console.log('subscribing to ', dao.name);
     const amount = 1e17;
     const flowRate = 1e14;
+
     await distribute(amount);
-    await createNewFlow(flowRate.toString());
+    // await createNewFlow(flowRate.toString());
     await loading(5);
   };
 
@@ -41,6 +42,73 @@ export default function Subscribe({ dao }: { dao: any }) {
           <Sparkle size={24} />
         </Flex>
       </Button>
+
+      <Box p={4} mt={6}>
+        <Text fontWeight='medium'>Based on your contribution level, benefits include: </Text>
+        <Flex gap={4} mt={4}>
+          <Box
+            w='160px'
+            h='100px'
+            display='grid'
+            placeContent='center'
+            bg={useColorModeValue('gray.200', 'gray.700')}
+            rounded='md'
+          >
+            <VStack>
+              <Chats size={32} />
+              <Text fontSize='md' fontWeight='medium'>
+                Chats
+              </Text>
+            </VStack>
+          </Box>
+          <Box
+            w='160px'
+            h='100px'
+            display='grid'
+            placeContent='center'
+            bg={useColorModeValue('gray.200', 'gray.700')}
+            rounded='md'
+          >
+            <VStack>
+              <Chats size={32} />
+              <Text fontSize='md' fontWeight='medium'>
+                NFTs
+              </Text>
+            </VStack>
+          </Box>
+          <Box
+            w='160px'
+            h='100px'
+            display='grid'
+            placeContent='center'
+            bg={useColorModeValue('gray.200', 'gray.700')}
+            rounded='md'
+          >
+            <VStack>
+              <Chats size={32} />
+              <Text fontSize='md' fontWeight='medium'>
+                Media
+              </Text>
+            </VStack>
+          </Box>
+          <Box
+            w='160px'
+            h='100px'
+            display='grid'
+            placeContent='center'
+            bg={useColorModeValue('gray.200', 'gray.700')}
+            rounded='md'
+          >
+            <VStack>
+              <Chats size={32} />
+              <Text fontSize='md' fontWeight='medium'>
+                Whitelists
+              </Text>
+            </VStack>
+          </Box>
+        </Flex>
+      </Box>
+      <Text fontWeight='medium'>And much more...</Text>
     </Box>
   );
 }
